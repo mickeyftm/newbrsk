8d4546d01ad829f4156629b63b05661760b2b56a
polyCENTdev <84585761+mickeyftm@users.noreply.github.com>
2021-06-23 04:42:41 +0900

    Update Dividend.tsx

--- src/views/Dividend/Dividend.tsx
+++ src/views/Dividend/Dividend.tsx
@@ -81,7 +81,7 @@ const Farm: React.FC = () => {
         </div>
         <img src="/images/syrup.png" alt="SYRUP POOL icon" width={410} height={191} />
       </Hero>
-      {/*} <CakeStats /> */}
+      {/* <CakeStats /> */}
       <br />
       <Heading as="h1" size="xl" mb="20px">
       <u>Please be sure to harvest your WMATIC before you unstake BRSK.</u>
